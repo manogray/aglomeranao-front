215c6445d25aff61aa725c0df71c24dda4b5782c
Luis Mota <lgmpf@icomp.ufam.edu.br>
2020-04-12 00:00:34 -0400

    Clean useless stuff

---
+++ src/pages/Home/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+import { Title2 } from './style';
+
+export default function Home() {
+  return (
+    <Title2>Aglomera Não</Title2>
+  );
+}
---
+++ src/pages/Home/style.js
@@ -0,0 +1,5 @@
+import styled from 'styled-components';
+
+export const Title2 = styled.h2`
+    font-family: 'Montserrat', sans-serif;
+`;
